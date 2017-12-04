9de130d65842ea2cc353c348697a396ec79df304
Kalebe <kalebemisael@gmail.com>
2017-12-04 08:58:11 -0200

    edit

--- PI.Interface/WindowsForms/PI.WindowsForms/Formularios/FormEstacionamento.Designer.cs
+++ PI.Interface/WindowsForms/PI.WindowsForms/Formularios/FormEstacionamento.Designer.cs
@@ -40,6 +40,7 @@
             this.btnExcluirEstacionamento = new System.Windows.Forms.Button();
             this.dataGridEstacionamento = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridEstacionamento)).BeginInit();
             this.SuspendLayout();
             // 
@@ -139,11 +140,21 @@
             this.button1.Text = "Excluir";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(234, 169);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Editar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // FormEstacionamento
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(542, 361);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridEstacionamento);
             this.Controls.Add(this.btnExcluirEstacionamento);
@@ -178,5 +189,6 @@
         private System.Windows.Forms.Button btnExcluirEstacionamento;
         private System.Windows.Forms.DataGridView dataGridEstacionamento;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
